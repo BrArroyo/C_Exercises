968b4830792e500f32f843e9b68da5efb5ebba31
bruno <bruno.lorenzo.arroyo@gmail.com>
2022-12-14 20:40:27 +0000

    first and second

---
+++ ejercice_2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// The main function
+int main(void) {
+  int number;  
+  printf("Introduzca el numero que quiera doblar: ");
+  scanf("%d", &number);
+   
+  printf("El doble del numero es : %d\n", number * 2); 
+
+  return 0;
+}
